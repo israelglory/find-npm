d32ea77ef6b3aad88c1f0c3d95685613f1009cac
israelglory <iglory6@gmail.com>
2023-07-09 04:20:58 +0100

    added toast to it

--- src/views/home.js
+++ src/views/home.js
@@ -4,6 +4,7 @@ import Loader from '../components/loader';
 import { useDispatch, useSelector } from "react-redux";
 import { search, searchData } from "../actions/action";
 import { useHistory } from "react-router-dom";
+import Toast from "../components/toast";
 
 function Home() {
     const [loading, setLoading] = useState(false);
@@ -11,23 +12,40 @@ function Home() {
     const dispatch = useDispatch();
     const items = useSelector(state => state.search);
     const history = useHistory();
+    const [showToast, setShowToast] = useState(false);
+    const [error, setError] = useState('Text Cannot be Empty');
+
+  const handleCloseToast = () => {
+    setShowToast(false);
+  };
     
 
   const fetchUserData = () => {
-    setLoading(true);
-    fetch("https://registry.npmjs.org/-/v1/search?text=" + text)
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        console.log(data['objects']);
-        //setUsers(data)
-        dispatch(searchData(data['objects']));
-        dispatch(search());
-        setLoading(false);
-        console.log(items);
-        history.push("/list");
-      })
+    if(text.length != 0){
+      setLoading(true);
+      fetch("https://registry.npmjs.org/-/v1/search?text=" + text)
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data['objects']);
+          //setUsers(data)
+          dispatch(searchData(data['objects']));
+          dispatch(search());
+          setLoading(false);
+          console.log(items);
+          history.push("/list");
+        }).catch(err => {
+          setError(err.message + ' Check Internet Connection');
+          setShowToast(true);
+          console.log(err);
+          setLoading(false);
+  })
+    }else{
+      setError('Text Cannot be Empty');
+      setShowToast(true);
+      setLoading(false);
+    }
   }
 
   return (
@@ -42,8 +60,13 @@ function Home() {
         <Loader />
          : 'Search'}
         </button>
+
       </div>
+      {showToast && (
+        <Toast message= {error} onClose={handleCloseToast} />
+      )}
     </div>
+
     </>
   );
 }
